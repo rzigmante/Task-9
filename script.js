0d104e88b122cac103427e47f0fc61afac0032c4
Raimonda Zigmante <raices7@yahoo.com>
2023-02-20 22:54:21 +0200

    Myf first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus su 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
+------------------------------------------------------------------------------------------------------ */
+
+class MovieES6 {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    return this.budget > 100000000;
+    console.log("true");
+  }
+}
+
+const movieContact = new MovieES6("Contact", "Robert Zemeckis", 90000000);
+console.log(movieContact.wasExpensive());
